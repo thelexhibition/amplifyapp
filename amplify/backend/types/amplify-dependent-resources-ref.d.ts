6e35226792bd815a1d97c27448a6d58e806d689e
asrasoft-admin <127885494+asrasoft-admin@users.noreply.github.com>
2023-08-09 23:37:29 +0500

    Delete amplify directory

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "amptest": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
